df348592b37f67cb526dbc4d9bb98f81945675b5
Tanya0609 <tanyi3025@gmail.com>
2019-05-23 11:28:06 -0700

    web

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+﻿const CONSTANTS = require("../constants");
+const express = require("express");
+const sampleData = require("../sampleData");
+
+
+const router = express.Router();
+// MasterDetail Page Endpoint
+router.get(CONSTANTS.ENDPOINT.MASTERDETAIL, (req, res) => {
+  res.set('Cache-Control', 'public, max-age=31557600');
+  res.json(sampleData.textAssets);
+});
+
+
+module.exports = router;
